825301ef7d55cf3c1f2f58892fb27d9ff971ef83
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-05-21 02:02:17 +0700

    fix

--- index.js
+++ index.js
@@ -69,19 +69,19 @@ checkbox.addEventListener('change', (e) => {
     form.innerHTML = `
       <div class="form-group">
             <label for="bhtInput">Требуемая сумма (в THB):</label>
-            <input type="number" step="0.01" class="form-control" id="bhtInput" name="bhtInput" required>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="bhtInput" name="bhtInput" required>
       </div>
       <div class="form-group">
-            <label for="usdThbRate">Курс THB > USD (<a target="_blank" href="https://www.visa.co.th/en_TH/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
-            <input type="number" step="0.01" class="form-control" id="usdThbRate" name="usdThbRate" required>
+            <label for="usdThbRate">Курс USD > THB (<a target="_blank" href="https://www.visa.co.th/en_TH/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdThbRate" name="usdThbRate" required>
       </div>
       <div class="form-group">
-            <label for="usdKztRate">Курс KZT > USD (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
-            <input type="number" step="0.01" class="form-control" id="usdKztRate" name="usdKztRate" required>
+            <label for="usdKztRate">Курс USD > KZT (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdKztRate" name="usdKztRate" required>
       </div>
       <div class="form-group">
-            <label for="kztRubRate">Курс KZT > RUB (<a target="_blank" href="https://www.finsend.io">finsend</a>):</label>
-            <input type="number" step="0.01" class="form-control" id="kztRubRate" name="kztRubRate" required>
+            <label for="kztRubRate">Курс RUB > KZT (<a target="_blank" href="https://www.finsend.io">finsend</a>):</label>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="kztRubRate" name="kztRubRate" required>
       </div>
       <button type="submit" class="btn btn-primary mt-4 mb-4">Рассчитать</button>
       <h2 id="result"></h2>
@@ -91,15 +91,15 @@ checkbox.addEventListener('change', (e) => {
     form.innerHTML = `
       <div class="form-group">
             <label for="bhtInput">Требуемая сумма (в THB):</label>
-            <input type="number" step="0.01" class="form-control" id="bhtInput" name="bhtInput" required>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="bhtInput" name="bhtInput" required>
       </div>
       <div class="form-group">
-            <label for="usdThbRate">Курс THB > USD (<a href="https://usa.visa.com/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
-            <input type="number" step="0.01" class="form-control" id="usdThbRate" name="usdThbRate" required>
+            <label for="usdThbRate">Курс USD > THB (<a href="https://usa.visa.com/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdThbRate" name="usdThbRate" required>
       </div>
       <div class="form-group">
-            <label for="usdRubRate">Курс RUB > USD (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
-            <input type="number" step="0.01" class="form-control" id="usdRubRate" name="usdRubRate" required>
+            <label for="usdRubRate">Курс USD > RUB (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
+            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdRubRate" name="usdRubRate" required>
       </div>
       <button type="submit" class="btn btn-primary mt-4 mb-4">Рассчитать</button>
       <h2 id="result"></h2>
