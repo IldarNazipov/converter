a32e823863a0793ad8bc0ef957bdedf09c90fd8e
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-05-21 16:58:57 +0700

    fix

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const calculateRateKzt = (curInput, usdCurRate, usdKztRate, kztRubRate) => {
   const curInUsd = curWithCommision / usdCurRate;
   const usdInKzt = usdKztRate * curInUsd;
   const kztInRub = usdInKzt / kztRubRate;
-  const result = kztInRub / curInput;
-  return result.toFixed(2);
+  const result = kztInRub;
+  return result;
 };
 
 const calculateRateRub = (curInput, usdCurRate, usdRubRate) => {
@@ -32,14 +32,13 @@ const calculateRateRub = (curInput, usdCurRate, usdRubRate) => {
   }
   const curInUsd = curWithCommision / usdCurRate;
   const usdInRub = usdRubRate * curInUsd;
-  const result = usdInRub / curInput;
-  return result.toFixed(2);
+  const result = usdInRub;
+  return result;
 };
 
 const form = document.getElementById('conversionForm');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const formData = new FormData(e.target);
   const curInput = Number(formData.get('curInput'));
   const usdCurRate = Number(formData.get('usdCurRate'));
@@ -49,8 +48,10 @@ form.addEventListener('submit', (e) => {
   const result = state.isChecked
     ? calculateRateKzt(curInput, usdCurRate, usdKztRate, kztRubRate)
     : calculateRateRub(curInput, usdCurRate, usdRubRate);
-  const h2 = document.querySelector('#result');
-  h2.textContent = `Курс к рублю: ${result}`;
+  const h2rate = document.querySelector('#result');
+  h2rate.textContent = `Курс к рублю: ${(result / curInput).toFixed(2)}`;
+  const h2sum = document.querySelector('#result-2');
+  h2sum.textContent = `Сумма в рублях: ${result.toFixed(2)}`;
 });
 
 const feeCheckbox = document.getElementById('feeCheckbox');
@@ -64,45 +65,27 @@ feeCheckbox.addEventListener('change', (e) => {
 
 const checkbox = document.getElementById('kztCheckbox');
 checkbox.addEventListener('change', (e) => {
+  const usdRubDiv = document.querySelector('.usd-rub');
+  const usdRubInput = document.getElementById('usdRubRate');
+  const usdKztDiv = document.querySelector('.usd-kzt');
+  const usdKztInput = document.getElementById('usdKztRate');
+  const kztRubDiv = document.querySelector('.kzt-rub');
+  const kztRubInput = document.getElementById('kztRubRate');
   if (e.target.checked) {
     state.isChecked = true;
-    form.innerHTML = `
-      <div class="form-group">
-            <label for="bhtInput">Требуемая сумма (в третьей валюте):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="bhtInput" name="bhtInput" required>
-      </div>
-      <div class="form-group">
-            <label for="usdThbRate">Курс USD к третьей валюте (<a target="_blank" href="https://www.visa.co.th/en_TH/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdThbRate" name="usdThbRate" required>
-      </div>
-      <div class="form-group">
-            <label for="usdKztRate">Курс USD к KZT (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdKztRate" name="usdKztRate" required>
-      </div>
-      <div class="form-group">
-            <label for="kztRubRate">Курс RUB к KZT (<a target="_blank" href="https://www.finsend.io">finsend</a>):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="kztRubRate" name="kztRubRate" required>
-      </div>
-      <button type="submit" class="btn btn-primary mt-4 mb-4">Рассчитать</button>
-      <h2 id="result"></h2>
-      `;
+    usdRubDiv.style.display = 'none';
+    usdRubInput.removeAttribute('required');
+    usdKztDiv.style.display = 'block';
+    usdKztInput.setAttribute('required', '');
+    kztRubDiv.style.display = 'block';
+    kztRubInput.setAttribute('required', '');
   } else {
     state.isChecked = false;
-    form.innerHTML = `
-      <div class="form-group">
-            <label for="bhtInput">Требуемая сумма (в третьей валюте):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="bhtInput" name="bhtInput" required>
-      </div>
-      <div class="form-group">
-            <label for="usdThbRate">Курс USD к третьей валюте (<a href="https://usa.visa.com/support/consumer/travel-support/exchange-rate-calculator.html">Visa</a>):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdThbRate" name="usdThbRate" required>
-      </div>
-      <div class="form-group">
-            <label for="usdRubRate">Курс USD к RUB (<a target="_blank" href="https://t.me/KZrobot">БЦК</a>):</label>
-            <input type="number" step="0.01" placeholder="0.00" class="form-control" id="usdRubRate" name="usdRubRate" required>
-      </div>
-      <button type="submit" class="btn btn-primary mt-4 mb-4">Рассчитать</button>
-      <h2 id="result"></h2>
-      `;
+    usdRubDiv.style.display = 'block';
+    usdRubInput.setAttribute('required', '');
+    usdKztDiv.style.display = 'none';
+    usdKztInput.removeAttribute('required');
+    kztRubDiv.style.display = 'none';
+    kztRubInput.removeAttribute('required');
   }
 });
