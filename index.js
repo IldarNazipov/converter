7834d3bfe455e018fa46c82ede17cf30960bdd4a
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-05-24 20:13:07 +0700

    fix

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 const state = {
   isKzt: false,
-  isFee: true,
+  isFee: false,
 };
 
-const calculateRateKzt = (curInput, usdCurRate, usdKztRate, kztRubRate) => {
+const calculateRate = (curInput, usdCurRate, usdRate, kztRubRate, outputCurrency, feeSum) => {
   let curWithCommision;
   if (state.isFee) {
-    curWithCommision =
-      curInput <= 30000
-        ? curInput + 220
-        : curInput + Math.ceil(curInput / 30000) * 220;
+    curWithCommision = curInput + feeSum; 
   } else {
     curWithCommision = curInput;
   }
   const curInUsd = curWithCommision / usdCurRate;
-  const usdInKzt = usdKztRate * curInUsd;
-  const kztInRub = usdInKzt / kztRubRate;
-  const result = kztInRub;
-  return result;
-};
 
-const calculateRateRub = (curInput, usdCurRate, usdRubRate) => {
-  let curWithCommision;
-  if (state.isFee) {
-    curWithCommision =
-      curInput <= 30000
-        ? curInput + 220
-        : curInput + Math.ceil(curInput / 30000) * 220;
+  const usdInOutputCurrency = usdRate * curInUsd;
+  let result;
+  if (outputCurrency === 'kzt') {
+    const kztInRub = usdInOutputCurrency / kztRubRate;
+    result = kztInRub;
+  } else if (outputCurrency === 'rub') {
+    result = usdInOutputCurrency;
   } else {
-    curWithCommision = curInput;
+    throw new Error('Invalid output currency');
   }
-  const curInUsd = curWithCommision / usdCurRate;
-  const usdInRub = usdRubRate * curInUsd;
-  const result = usdInRub;
+
   return result;
 };
 
 const form = document.getElementById('conversionForm');
+const select = document.getElementById('select-addon');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
@@ -45,26 +36,34 @@ form.addEventListener('submit', (e) => {
   const usdRubRate = Number(formData.get('usdRubRate'));
   const usdKztRate = Number(formData.get('usdKztRate'));
   const kztRubRate = Number(formData.get('kztRubRate'));
-  const result = state.isChecked
-    ? calculateRateKzt(curInput, usdCurRate, usdKztRate, kztRubRate)
-    : calculateRateRub(curInput, usdCurRate, usdRubRate);
-  const h2rate = document.querySelector('#result');
-  h2rate.textContent = `Курс к рублю: ${(result / curInput).toFixed(2)}`;
-  const h2sum = document.querySelector('#result-2');
-  h2sum.textContent = `Сумма в рублях: ${result.toFixed(2)}`;
+  const feeInput = Number(formData.get('feeInput'));
+  const feeSum = select.value === 'sum' ? feeInput : feeInput * curInput / 100;
+  const result = state.isKzt
+    ? calculateRate(curInput, usdCurRate, usdKztRate, kztRubRate, 'kzt', feeSum)
+    : calculateRate(curInput, usdCurRate, usdRubRate, kztRubRate, 'rub', feeSum);
+  const rateResultEl = document.querySelector('#result');
+  rateResultEl.textContent = `Курс к рублю: ${(result / curInput).toFixed(2)}`;
+  const sumResultEl = document.querySelector('#result-2');
+  sumResultEl.textContent = `Сумма в рублях: ${result.toFixed(2)}`;
 });
 
 const feeCheckbox = document.getElementById('feeCheckbox');
 feeCheckbox.addEventListener('change', (e) => {
+  const feeDiv = document.querySelector('.fee');
+  const feeInput = document.getElementById('feeInput');
   if (e.target.checked) {
     state.isFee = true;
+    feeDiv.style.display = 'flex';
+    feeInput.setAttribute('required', '');
   } else {
     state.isFee = false;
+    feeDiv.style.display = 'none';
+    feeInput.removeAttribute('required');
   }
 });
 
-const checkbox = document.getElementById('kztCheckbox');
-checkbox.addEventListener('change', (e) => {
+const kztCheckbox = document.getElementById('kztCheckbox');
+kztCheckbox.addEventListener('change', (e) => {
   const usdRubDiv = document.querySelector('.usd-rub');
   const usdRubInput = document.getElementById('usdRubRate');
   const usdKztDiv = document.querySelector('.usd-kzt');
@@ -72,7 +71,7 @@ checkbox.addEventListener('change', (e) => {
   const kztRubDiv = document.querySelector('.kzt-rub');
   const kztRubInput = document.getElementById('kztRubRate');
   if (e.target.checked) {
-    state.isChecked = true;
+    state.isKzt = true;
     usdRubDiv.style.display = 'none';
     usdRubInput.removeAttribute('required');
     usdKztDiv.style.display = 'block';
@@ -80,7 +79,7 @@ checkbox.addEventListener('change', (e) => {
     kztRubDiv.style.display = 'block';
     kztRubInput.setAttribute('required', '');
   } else {
-    state.isChecked = false;
+    state.isKzt = false;
     usdRubDiv.style.display = 'block';
     usdRubInput.setAttribute('required', '');
     usdKztDiv.style.display = 'none';
